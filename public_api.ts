16c6d491f35ed6cd2ace91b08248a90413df01b2
Marco Amadori <mamadori@vivocha.com>
2018-04-06 13:05:15 +0200

    exposed few components

--- public_api.ts
+++ public_api.ts
@@ -8,3 +8,7 @@ export {MinimizedModule} from './src/modules/minimized/minimized.module';
 export {MultimediaModule} from './src/modules/multimedia/multimedia.module';
 export {QueueModule} from './src/modules/queue/queue.module';
 export {TopBarModule} from './src/modules/top-bar/top-bar.module';
+
+export {ChatAreaComponent} from './src/modules/chat/chat-area/chat-area.component';
+export {TopBarComponent} from './src/modules/top-bar/top-bar/top-bar.component';
+export {ChatIsWritingComponent} from './src/modules/chat/chat-is-writing/chat-is-writing.component';
