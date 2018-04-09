602150cb2e8d96f2272cd4c441c843f49d934ee9
Marco Amadori <mamadori@vivocha.com>
2018-04-09 15:35:06 +0200

    test without debug component

--- public_api.ts
+++ public_api.ts
@@ -1,4 +1,3 @@
 export {InteractionCoreModule} from './src/modules/interaction-core/interaction-core.module';
-export {InteractionCoreDebugModule} from './src/modules/interaction-core/interaction-core-debug.module';
 export {VvcInteractionService} from './src/modules/interaction-core/services/interaction.service';
 export * from './src/modules/interaction-core/store/models.interface';
