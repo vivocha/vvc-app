eda3b700a59cf3eb87aa2b97a4a6185459786538
Marco Amadori <mamadori@vivocha.com>
2018-04-11 14:47:58 +0200

    removed _mocks directory with obsolete files

--- _mocks/translate-mock.pipe.ts
+++
@@ -1,8 +0,0 @@
-import {Pipe, PipeTransform} from '@angular/core';
-
-@Pipe({name: 'translate'})
-export class MockTranslate implements PipeTransform {
-    transform(value: number): number {
-        return value;
-    }
-}
