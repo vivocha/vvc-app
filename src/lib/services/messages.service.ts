fa54dd3d2ef2f398524da52c0e2dfb33098e7376
Marco Amadori <mamadori@vivocha.com>
2018-08-06 18:22:07 +0200

    linted and added scrollLeft to quick reply to better scrolling behaviour

--- src/lib/services/messages.service.ts
+++ src/lib/services/messages.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {Store} from '@ngrx/store';
 
 import {AppState} from '../store/reducers/main.reducer';
-import {NewMessage,RemoveMessage,UpdateMessage} from '../store/actions/messages.actions';
+import {NewMessage, RemoveMessage, UpdateMessage} from '../store/actions/messages.actions';
 import {SystemMessage, ChatMessage, RequestMessage, LinkMessage, LeftScrollOffset} from '../store/models.interface';
 
 @Injectable()
@@ -10,49 +10,54 @@ export class VvcMessageService {
 
   constructor(
     private store: Store<AppState>
-  ){}
+  ) {}
 
-  addChatMessage(message, agent?, visitorNick?){
+  addChatMessage(message, agent?, visitorNick?) {
     const id = new Date().getTime().toString();
-    const msg:ChatMessage = {
+    const msg: ChatMessage = {
       id: id,
       text: message.body,
       type: 'chat',
       isAgent: agent,
       time: this.getChatTimestamp(message.ts)
     };
-    if (agent) msg.agent = agent;
-    if (message.meta) msg.meta = message.meta;
-    if (visitorNick) msg.visitorNick = visitorNick;
+    if (agent) {
+      msg.agent = agent;
+    }
+    if (message.meta) {
+      msg.meta = message.meta;
+    }
+    if (visitorNick) {
+      msg.visitorNick = visitorNick;
+    }
     this.store.dispatch(new NewMessage(msg));
     return id;
   }
-  addCustomMessage(message){
+  addCustomMessage(message) {
     const id = new Date().getTime().toString();
     const m = {
       id: id,
-      code: "message",
-      type: "custom",
+      code: 'message',
+      type: 'custom',
       body: { ...message }
     };
     this.store.dispatch(new NewMessage(m));
     return id;
   }
-  addDialogMessage(message, agent?){
-    if (message.quick_replies){
+  addDialogMessage(message, agent?) {
+    if (message.quick_replies) {
       this.addQuickRepliesMessage(message, agent);
-    }
-    else if (message.template) {
+    } else if (message.template) {
       this.addTemplateMessage(message, agent);
     } else {
       this.addChatMessage(message, agent);
     }
   }
-  addLocalMessage(text){
+  addLocalMessage(text) {
     const id = new Date().getTime().toString();
     const msg: ChatMessage = {
       id: id,
-      text:text,
+      text: text,
       type: 'chat',
       isAgent: false,
       time: this.getChatTimestamp()
@@ -60,11 +65,11 @@ export class VvcMessageService {
     this.store.dispatch(new NewMessage(msg));
     return id;
   }
-  addLinkMessage(url: string, from_id: string, from_nick: string, desc?: string, agent?: boolean){
+  addLinkMessage(url: string, from_id: string, from_nick: string, desc?: string, agent?: boolean) {
     const id = new Date().getTime().toString();
     const msg: LinkMessage = {
       id: id,
-      text:desc || url,
+      text: desc || url,
       url: url,
       from_id: from_id,
       from_nick: from_nick,
@@ -74,22 +79,25 @@ export class VvcMessageService {
     this.store.dispatch(new NewMessage(msg));
     return id;
   }
-  addQuickRepliesMessage(message, agent?){
+  addQuickRepliesMessage(message, agent?) {
     const id = new Date().getTime().toString();
     const quick: any = {
       id: id,
-      code: "message",
-      type: "quick-replies",
+      code: 'message',
+      type: 'quick-replies',
       body: message.body,
       quick_replies: message.quick_replies,
       quick_replies_orientation: message.quick_replies_orientation,
+      scrollLeft: 0,
       time: this.getChatTimestamp()
     };
-    if (agent) quick.agent = agent;
+    if (agent) {
+      quick.agent = agent;
+    }
     this.store.dispatch(new NewMessage(quick));
     return id;
   }
-  addTemplateMessage(message, agent?){
+  addTemplateMessage(message, agent?) {
     const id = new Date().getTime().toString();
     const template: any = {
       id: id,
@@ -100,24 +108,29 @@ export class VvcMessageService {
       scrollLeft: 0,
       time: this.getChatTimestamp()
     };
-    if (agent) template.agent = agent;
+    if (agent) {
+      template.agent = agent;
+    }
     this.store.dispatch(new NewMessage(template));
     return id;
   }
-  getChatTimestamp(tsString?: string){
+  getChatTimestamp(tsString?: string) {
     let t;
-    if (tsString) t = new Date(tsString);
-    else t = new Date();
-    const h = (parseInt(t.getHours()) > 9) ? t.getHours() : "0"+t.getHours();
-    const m = (parseInt(t.getMinutes()) > 9) ? t.getMinutes() : "0"+t.getMinutes();
-    return h + ":" + m;
+    if (tsString) {
+      t = new Date(tsString);
+    } else {
+      t = new Date();
+    }
+    const h = (parseInt(t.getHours()) > 9) ? t.getHours() : '0' + t.getHours();
+    const m = (parseInt(t.getMinutes()) > 9) ? t.getMinutes() : '0' + t.getMinutes();
+    return h + ':' + m;
   }
-  removeMessage(messageId: string){
+  removeMessage(messageId: string) {
     this.store.dispatch(new RemoveMessage(messageId));
   }
-  sendRequestMessage(message){
+  sendRequestMessage(message) {
     const id = new Date().getTime().toString();
-    let m: RequestMessage = {
+    const m: RequestMessage = {
       id: id,
       type: 'request',
       text: message.toUpperCase()
@@ -125,24 +138,23 @@ export class VvcMessageService {
     this.store.dispatch(new NewMessage(m));
     return id;
   }
-  sendSystemMessage(messageNameId: string, context?: any){
+  sendSystemMessage(messageNameId: string, context?: any) {
     const id = new Date().getTime().toString();
-    let message: SystemMessage = {
+    const message: SystemMessage = {
       id: id,
       type: 'system',
       text: messageNameId
     };
-    if (context){
+    if (context) {
       message.context = context;
     }
     this.store.dispatch(new NewMessage(message));
     return id;
   }
-  updateQuickReply(messageId){
-    this.store.dispatch(new UpdateMessage({ id: messageId, patch: { replied : true }}))
+  updateQuickReply(messageId) {
+    this.store.dispatch(new UpdateMessage({ id: messageId, patch: { replied : true }}));
   }
-  updateLeftScroll(o: LeftScrollOffset){
-    console.log('patching left scroll', o);
+  updateLeftScroll(o: LeftScrollOffset) {
     this.store.dispatch(new UpdateMessage( {id: o.messageId, patch: { scrollLeft: o.scrollLeft }}));
   }
 }
