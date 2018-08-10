902aef2dfaadac2813f750e365675c3b9a3c08a1
Marco Amadori <mamadori@vivocha.com>
2018-08-10 17:24:12 +0200

    screen sharing management

--- src/lib/multimedia/full-screen-video/full-screen-video.component.ts
+++ src/lib/multimedia/full-screen-video/full-screen-video.component.ts
@@ -15,15 +15,60 @@ export class FullScreenVideoComponent {
   @Output() hangUp = new EventEmitter();
   @Output() normalScreen = new EventEmitter();
 
-  hideVideo = false;
+  hideLocalVideo = false;
+  hideRemoteVideo = false;
+
+  switchVideo = false;
 
   constructor(private sanitizer: DomSanitizer) { }
 
   chatToggle() {
     this.displayChat.emit(!this.context.showChatOnFullScreen);
   }
+  getSrcForVideo(type: 'big' | 'local' | 'remote') {
+    switch (type) {
+      case 'big':
+        if (this.switchVideo) {
+          return this.context.videoRxStream || this.context.screenRxStream || this.context.videoTxStream;
+        } else {
+          return this.context.screenRxStream || this.context.videoRxStream || this.context.videoTxStream;
+        }
+      case 'local':
+        return this.context.videoTxStream;
+      case 'remote':
+        if (this.switchVideo) {
+          return this.context.screenRxStream || this.context.videoRxStream;
+        } else {
+          return this.context.videoRxStream;
+        }
+    }
+  }
   hangup() {
     this.hangUp.emit();
+  }
+  isMediaToolbarVisible() {
+    return (this.context.videoRxStream || this.context.videoTxStream || this.context.voiceRxStream);
+  }
+  isFlipped() {
+    return !this.context.screenRxStream && !this.context.videoRxStream && this.context.videoTxStream;
+  }
+  isVideoVisible(type: 'big' | 'local' | 'remote') {
+    switch (type) {
+      case 'big':
+        return (
+          this.context.screenRxStream ||
+          this.context.videoRxStream  ||
+          (!this.context.screenRxStream && !this.context.videoRxStream && this.context.videoTxStream)
+        );
+      case 'local':
+        return (
+          this.context.videoTxStream && (this.context.screenRxStream || this.context.videoRxStream)
+        );
+      case 'remote':
+        return (
+          this.context.screenRxStream && this.context.videoRxStream
+        );
+    }
   }
   toggleMute() {
     this.muteToggle.emit(!this.context.isMuted);
--- src/lib/multimedia/media/media.component.ts
+++ src/lib/multimedia/media/media.component.ts
@@ -16,7 +16,10 @@ export class MediaComponent {
   @Output() displayChat = new EventEmitter();
   @Output() hangUp = new EventEmitter();
 
-  hideVideo = false;
+  hideLocalVideo = false;
+  hideRemoteVideo = false;
+
+  switchVideo = false;
 
   constructor(private sanitizer: DomSanitizer) { }
 
@@ -25,6 +28,48 @@ export class MediaComponent {
   }
   hangup() {
     this.hangUp.emit();
+  }
+  getSrcForVideo(type: 'big' | 'local' | 'remote') {
+    switch (type) {
+      case 'big':
+        if (this.switchVideo) {
+          return this.context.videoRxStream || this.context.screenRxStream || this.context.videoTxStream;
+        } else {
+          return this.context.screenRxStream || this.context.videoRxStream || this.context.videoTxStream;
+        }
+      case 'local':
+        return this.context.videoTxStream;
+      case 'remote':
+        if (this.switchVideo) {
+          return this.context.screenRxStream || this.context.videoRxStream;
+        } else {
+          return this.context.videoRxStream;
+        }
+    }
+  }
+  isMediaToolbarVisible() {
+    return (this.context.videoRxStream || this.context.videoTxStream || this.context.voiceRxStream);
+  }
+  isFlipped() {
+    return !this.context.screenRxStream && !this.context.videoRxStream && this.context.videoTxStream;
+  }
+  isVideoVisible(type: 'big' | 'local' | 'remote') {
+    switch (type) {
+      case 'big':
+        return (
+          this.context.screenRxStream ||
+          this.context.videoRxStream  ||
+          (!this.context.screenRxStream && !this.context.videoRxStream && this.context.videoTxStream)
+        );
+      case 'local':
+        return (
+          this.context.videoTxStream && (this.context.screenRxStream || this.context.videoRxStream)
+        );
+      case 'remote':
+        return (
+          this.context.screenRxStream && this.context.videoRxStream
+        );
+    }
   }
   rejectIncomingOffer() {
     this.onReject.emit();
