6bc731873bce7f2c1b626023cda66535f0ea5538
Andrea Lovicu <alovicu@vivocha.com>
2019-11-14 12:25:07 +0100

    chore(interaction.core): remove old build scripts

--- interaction/lib/core/fixdeps.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-const fs = require('fs');
-const packageJson = require('./dist/package.json');
-
-delete packageJson['devDependencies'];
-delete packageJson['dependencies'];
-fs.writeFileSync('dist/package.json', JSON.stringify(packageJson, undefined, 2));
