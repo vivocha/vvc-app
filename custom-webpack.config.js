10e4223a6f07208ac3af863c545fdbd5144f405e
Andrea Lovicu <alovicu@vivocha.com>
2019-11-15 18:17:36 +0100

    fix(interaction.app): disable alias

--- custom-webpack.config.js
+++ custom-webpack.config.js
@@ -1,4 +1,4 @@
 module.exports = (config, options) => {
-  config.resolve.alias['@vivocha/public-entities/dist/wrappers/data_collection'] = '@vivocha/public-entities/dist/es5/wrappers/data_collection';
+  //config.resolve.alias['@vivocha/public-entities/dist/wrappers/data_collection'] = '@vivocha/public-entities/dist/es5/wrappers/data_collection';
   return config;
 };
