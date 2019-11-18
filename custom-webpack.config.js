4b5a817f9264c866b73cc1271a3d4451432b382d
Andrea Lovicu <alovicu@vivocha.com>
2019-11-18 11:54:45 +0100

    fix(interaction.app): use babel to transpile es6 modules for IE11

--- custom-webpack.config.js
+++ custom-webpack.config.js
@@ -1,4 +1,20 @@
 module.exports = (config, options) => {
-  //config.resolve.alias['@vivocha/public-entities/dist/wrappers/data_collection'] = '@vivocha/public-entities/dist/es5/wrappers/data_collection';
+  config.module.rules.push(
+    {
+      test: /\.js$/,
+      exclude: /node_modules(?!\/whatwg\-fetch|\/webrtc\-adapter|\/fse|\/debuggo|\/debug|\/jsonref|\/\@vivocha)/,
+      loader: 'babel-loader',
+      query: {
+        cacheDirectory: false,
+        presets: [
+          ['@babel/env', {
+            targets: [
+              "ie 11"
+            ]
+          }]
+        ]
+      }
+    }
+  );
   return config;
 };
