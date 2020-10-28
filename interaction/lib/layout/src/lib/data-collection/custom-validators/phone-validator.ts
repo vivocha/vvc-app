a540a9724c1ca76804066054ec7f9934e7b3a458
Gianfranco Frau <gfrau@vivocha.com>
2020-10-28 11:43:32 +0100

    fix(layout): validate phonenum field when has a value
    fix #3

--- interaction/lib/layout/src/lib/data-collection/custom-validators/phone-validator.ts
+++ interaction/lib/layout/src/lib/data-collection/custom-validators/phone-validator.ts
@@ -1,10 +1,13 @@
-import {AbstractControl, ValidatorFn} from '@angular/forms';
-import {parsePhoneNumberFromString} from 'libphonenumber-js';
+import { AbstractControl, ValidatorFn } from '@angular/forms';
+import { parsePhoneNumberFromString } from 'libphonenumber-js';
 
 export function phoneNumberValidator(country): ValidatorFn {
-  return  (control: AbstractControl): {[key: string]: any} | null => {
-    const phoneNumber = control.value;
-    const phone = parsePhoneNumberFromString(phoneNumber, country.toUpperCase());
-    return (!phone || (phone && !phone.isValid())) ? { 'invalidPhoneNumber': { value: control.value }} : null;
-  }
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    if (control.value) {
+      const phoneNumber = control.value;
+      const phone = parsePhoneNumberFromString(phoneNumber, country.toUpperCase());
+      return !phone || (phone && !phone.isValid()) ? { invalidPhoneNumber: { value: control.value } } : null;
+    }
+    return null;
+  };
 }
